3bf45c4cc75de24b6cc7597ec8b822397341ccbc
Mehrab Rahman <30425043+MehrabRahman@users.noreply.github.com>
2021-04-17 04:53:17 -0500

    Update App.java

--- src/main/java/embedded/tomcat/spring/App.java
+++ src/main/java/embedded/tomcat/spring/App.java
@@ -18,12 +18,15 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
 @RestController
-public class App implements WebApplicationInitializer {
+public class App implements WebApplicationInitializer, WebMvcConfigurer {
     public static void main(String[] args) throws LifecycleException {
         Tomcat server = new Tomcat();
         server.setPort(8080);
@@ -43,6 +46,18 @@ public class App implements WebApplicationInitializer {
                 new DispatcherServlet(new GenericWebApplicationContext()));
         dispatcherServlet.setLoadOnStartup(1);
         dispatcherServlet.addMapping("/");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+          .addResourceHandler("/**")
+          .addResourceLocations("classpath:/static/");	
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
     }
 
     @GetMapping("/hello")
